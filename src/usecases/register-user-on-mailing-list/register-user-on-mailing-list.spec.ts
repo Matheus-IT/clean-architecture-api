7189d458b91b7a8e15148d3d7b5df95996e30ed5
matheus costa <matheuscosta.tech@gmail.com>
2022-01-14 21:42:05 -0300

    feat: add user data interface

---
+++ src/usecases/register-user-on-mailing-list/register-user-on-mailing-list.spec.ts
@@ -0,0 +1,17 @@
+import { UserData } from './user-data';
+
+describe('register user on mailing list use case', () => {
+	test('should add user with complete data to mailing list', async () => {
+		const users: UserData[] = [];
+		console.log(users);
+		// const repo: UserRepository = new InMemoryUserRepository(users);
+
+		// const usecase: RegisterUserOnMailinList = new RegisterUserOnMailingList(repo);
+		// const name = 'any_name';
+		// const email = 'any@email.com';
+		// const res = await usecase.RegisterUserOnMailingList({ name, email});
+
+		// const user = repo.findUserByEmail('any@email.com');
+		// expect((await user).name).toBe('any_name');
+	});
+});
