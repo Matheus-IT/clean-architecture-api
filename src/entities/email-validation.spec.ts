d2f2e89f7a3ffaa38000fd02d7e54a3c673e6b25
matheus costa <matheuscosta.tech@gmail.com>
2022-01-18 10:08:26 -0300

    test: validate aditional invalid emails

--- src/entities/email-validation.spec.ts
+++ src/entities/email-validation.spec.ts
@@ -31,6 +31,16 @@ describe('Email validation', () => {
 		expect(Email.validate(email)).toBeFalsy();
 	});
 
+	test('should not accept local part with ending dot', () => {
+		const email = 'invalid.@email.com';
+		expect(Email.validate(email)).toBeFalsy();
+	});
+
+	test('should accept domai with more than one dot', () => {
+		const email = 'juvenal@email.domain.com';
+		expect(Email.validate(email)).toBeTruthy();
+	});
+
 	test('should not accept domain larger than 190 chars', () => {
 		const email = 'l'.repeat(64) + '@' + 'd'.repeat(191); // one more char than acceptable
 		expect(Email.validate(email)).toBeFalsy();
@@ -43,6 +53,10 @@ describe('Email validation', () => {
 
 	test('should not accept email larger than 256 chars', () => {
 		const email = 'e'.repeat(257); // one more char than acceptable
+		expect(Email.validate(email)).toBeFalsy();
+	});
+	test('should not accept email without at-sign', () => {
+		const email = 'invalidemail.com';
 		expect(Email.validate(email)).toBeFalsy();
 	});
 });
