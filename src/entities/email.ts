a451952951bebf1d3e38039d1a4687fd5de6cfa2
matheus costa <matheuscosta.tech@gmail.com>
2022-01-18 09:24:40 -0300

    feat: validate email local part and domain length

--- src/entities/email.ts
+++ src/entities/email.ts
@@ -6,8 +6,8 @@ export class Email {
 		if (email.length > 256) {
 			return false;
 		}
-		const [local] = email.split('@');
-		if (local.length > 64) {
+		const [local, domain] = email.split('@');
+		if (local.length > 64 || domain.length > 191) {
 			return false;
 		}
 		return true;
