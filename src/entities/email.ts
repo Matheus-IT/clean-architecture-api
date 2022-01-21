de528b3acf6ee8dcda826cb5ad9d707e4f26cd55
matheus costa <matheuscosta.tech@gmail.com>
2022-01-20 21:35:17 -0300

    refactor: rename email to value on email value object

--- src/entities/email.ts
+++ src/entities/email.ts
@@ -2,33 +2,33 @@ import { Either, left, right } from '../shared/either';
 import { InvalidEmailError } from './errors/invalid-email-error';
 
 export class Email {
-	private readonly email: string;
+	private readonly value: string;
 
-	private constructor(email: string) {
-		this.email = email;
+	private constructor(value: string) {
+		this.value = value;
 	}
 
-	static create(email: string): Either<InvalidEmailError, Email> {
-		if (Email.validate(email)) {
-			return right(new Email(email));
+	static create(value: string): Either<InvalidEmailError, Email> {
+		if (Email.validate(value)) {
+			return right(new Email(value));
 		}
 		return left(new InvalidEmailError());
 	}
 
-	static validate(email: string): boolean {
+	static validate(value: string): boolean {
 		const emailRegex =
 			/^[-!#$%&'*+/0-9=?A-Z^_a-z`{|}~](\.?[-!#$%&'*+/0-9=?A-Z^_a-z`{|}~])*@[a-zA-Z0-9](-*\.?[a-zA-Z0-9])*\.[a-zA-Z](-?[a-zA-Z0-9])+$/;
 
-		if (!emailRegex.test(email)) {
+		if (!emailRegex.test(value)) {
 			return false;
 		}
-		if (!email) {
+		if (!value) {
 			return false;
 		}
-		if (email.length > 256) {
+		if (value.length > 256) {
 			return false;
 		}
-		const [local, domain] = email.split('@');
+		const [local, domain] = value.split('@');
 		if (local.length > 64 || local.length == 0 || domain.length > 190 || domain.length == 0) {
 			return false;
 		}
