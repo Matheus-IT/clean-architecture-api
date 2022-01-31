268e740c7c6dd95e3e47f3e6d635b90f85e1f38e
matheus costa <matheuscosta.tech@gmail.com>
2022-01-30 22:26:37 -0300

    feat: web controller should return 500 when server raises exception

--- src/web-controllers/register-user-controller.ts
+++ src/web-controllers/register-user-controller.ts
@@ -1,36 +1,40 @@
 import { UserData } from '@/entities';
-import { RegisterUserOnMailingList } from '@/usecases/register-user-on-mailing-list';
+import { UseCase } from '@/usecases/ports';
 import { HttpRequest, HttpResponse } from '@/web-controllers/ports';
-import { created, badRequest } from '@/web-controllers/utils';
+import { created, badRequest, serverError } from '@/web-controllers/utils';
 import { MissingParamError } from './errors';
 
 export class RegisterUserController {
-	private readonly usecase: RegisterUserOnMailingList;
+	private readonly usecase: UseCase;
 	private readonly requiredParams = ['name', 'email'];
 
-	constructor(usecase: RegisterUserOnMailingList) {
+	constructor(usecase: UseCase) {
 		this.usecase = usecase;
 	}
 
 	public async handle(request: HttpRequest): Promise<HttpResponse> {
-		const missingParams = [];
+		try {
+			const missingParams = [];
 
-		for (const el of this.requiredParams) {
-			if (!(el in request.body)) {
-				missingParams.push(el);
+			for (const el of this.requiredParams) {
+				if (!(el in request.body)) {
+					missingParams.push(el);
+				}
 			}
-		}
-		if (missingParams.length > 0) {
-			const missingParamsRepresentation = missingParams.join(' and ');
-			return badRequest(new MissingParamError(missingParamsRepresentation));
-		}
+			if (missingParams.length > 0) {
+				const missingParamsRepresentation = missingParams.join(' and ');
+				return badRequest(new MissingParamError(missingParamsRepresentation));
+			}
 
-		const userData: UserData = request.body;
-		const response = await this.usecase.perform(userData);
+			const userData: UserData = request.body;
+			const response = await this.usecase.perform(userData);
 
-		if (response.isLeft()) {
-			return badRequest(response.value);
+			if (response.isLeft()) {
+				return badRequest(response.value);
+			}
+			return created(response.value);
+		} catch (error) {
+			return serverError(error);
 		}
-		return created(response.value);
 	}
 }
