b3b942eb1da82ba31505d0885f194394aae4865f
matheus costa <matheuscosta.tech@gmail.com>
2022-03-12 21:49:06 -0300

    refactor: using dotenv variable

--- src/main/server.ts
+++ src/main/server.ts
@@ -1,8 +1,8 @@
 import 'module-alias/register';
 import { MongoHelper } from '@/external/repositories/mongodb/helper';
 
-MongoHelper.connect('mongodb://localhost').then(async () => {
-	const app = await (await import('./config/app')).default;
+MongoHelper.connect(process.env.MONGO_URL).then(async () => {
+	const app = (await import('./config/app')).default;
 	app.listen(5000, () => {
 		console.log('Server running at http://localhost:5000');
 	});
