a812447946d9c3fd3681e4326d15acfe354678a6
matheus costa <matheuscosta.tech@gmail.com>
2022-03-12 22:43:56 -0300

    fix: app port

--- src/main/server.ts
+++ src/main/server.ts
@@ -3,7 +3,7 @@ import { MongoHelper } from '@/external/repositories/mongodb/helper';
 
 MongoHelper.connect(process.env.MONGO_URL).then(async () => {
 	const app = (await import('./config/app')).default;
-	app.listen(5000, () => {
-		console.log('Server running at http://localhost:5000');
+	app.listen(process.env.PORT || 5000, () => {
+		console.log('Server running...');
 	});
 });
